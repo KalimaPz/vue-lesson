9211f4926aeb02214e74ea685f531fff1605463a
Donnukrit Satirakul <kalima007c@gmail.com>
2020-08-15 01:23:00 +0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+ <div id="app">
+  <!-- <p>
+
+    <router-link to="/foo">Go to sFoo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+  </p> -->
+
+  <router-view>
+
+  </router-view>
+</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios'
+// import Home from './views/Home.vue'
+export default {
+  name: "App",
+  
+  components: {
+    // Home
+  },
+  mounted() {
+    this.fetchApi()
+  },
+  data: () => ({
+    gitApiData : {}
+    
+  }),
+  methods : {
+  async fetchApi () {
+    await axios.get(`https://api.github.com/users/KalimaPz`)
+    .then((res)=>{
+      if(res.statusText == "OK" && res.status == 200)
+      // console.log(res.statusText)
+      // console.log(res.status)
+      // console.log(res.data.login)
+      this.gitApiData = res.data
+    })
+    .catch(() => {
+      console.log('ERROR')
+    }) 
+    
+  }
+  }
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
+<style scoped>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
